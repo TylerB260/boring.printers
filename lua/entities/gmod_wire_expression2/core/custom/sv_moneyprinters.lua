0a9dda30ae193b683b8cac76374b881888772bf3
Tyler Bundy <me@tylerbundy.me>
2018-07-21 00:54:13 -0500

    addl functions

--- lua/entities/gmod_wire_expression2/core/custom/sv_moneyprinters.lua
+++ lua/entities/gmod_wire_expression2/core/custom/sv_moneyprinters.lua
@@ -20,3 +20,23 @@ e2function normal entity:printerGetRate(string stat)
 	if not IsValid(this) then return 0 end
 	if this.GetRate then return this:GetRate(string.lower(stat)) end
 end
+
+e2function vector entity:printerGetFanPos()
+	if not IsValid(this) then return Vector() end
+	if this.GetFanPos then return this:GetFanPos() end
+end
+
+e2function vector entity:printerGetFanSize()
+	if not IsValid(this) then return Vector() end
+	if this.GetFanSize then return this:GetFanSize() end
+end
+
+e2function vector entity:printerGetButtonPos()
+	if not IsValid(this) then return Vector() end
+	if this.GetButtonPos then return this:GetButtonPos() end
+end
+
+e2function vector entity:printerGetFanSize()
+	if not IsValid(this) then return Vector() end
+	if this.GetButtonSize then return this:GetButtonSize() end
+end
