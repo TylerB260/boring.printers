f99446297c42223725f2e463a02dc17c46612658
Tyler Bundy <me@tylerbundy.me>
2018-07-21 00:00:07 -0500

    ffs

--- lua/entities/bb_printer_base/init.lua
+++ lua/entities/bb_printer_base/init.lua
@@ -72,8 +72,8 @@ function ENT:Think() -- increase stuff based on tickrate!!
 	
 	if self:WaterLevel() >= 1 then
 		local effectdata = EffectData()
-		effectdata:SetStart(self:GetFanPos())
-		effectdata:SetOrigin(self:GetFanPos())
+		effectdata:SetStart(self:GetPos())
+		effectdata:SetOrigin(self:GetPos())
 		effectdata:SetScale(1)
 		effectdata:SetMagnitude(1)
 		effectdata:SetScale(3)
